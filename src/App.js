78cab41b55cc70a225e72a940c184f82948c462e
jangthao <dangthao.stuk14@gmail.com>
2020-03-11 00:32:00 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const resolvable = <h1>Start with React</h1>;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {resolvable}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
